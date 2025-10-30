9e658d53dde567e0d8d1fac32acfd76cd5bc024a
jong-jin624 <jongjin1004baek@gmail.com>
2025-10-30 10:10:51 +0900

    퀴즈1 변수 기초

---
+++ src/be_study/quiz/quiz01.java
@@ -0,0 +1,53 @@
+package be_study.quiz;
+
+public class quiz01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int month;
+		int day = 1;
+		
+		month = 10;
+		day = 30;
+		System.out.println("오늘은 " + month + "월 " + day + "일 입니다.");
+		
+		String myName = "성이름";
+		String phoneNumber = "010-1234-5678";
+		
+		System.out.println(myName);
+		System.out.println("전화번호는 " + phoneNumber + " 입니다.");
+		
+		//6. 다음중 변수명으로 사용할 수 있는 것은?
+//		사용할 수 없는 경우 : 잘못된 사유를 옆에 함께 작성
+//		사용할 수 있으나 잘못된 부분이 있으면 해당 사유를 함께 작성
+//		a. $ystem O
+//		b. channel#112 X 특수문자는 $와 _만 가능하다.
+//		c. 7eleven	X 첫문자는 숫자로 시작할 수 없다.
+//		d. newmember O newMember
+//		e. $MAX_NUM O
+//		f. hello@com X 특수문자는 $와 _만 가능하다.
+//		g. int	X 자바 예약어(int 변수타입 예약어)는 사용할 수 없다.
+//		답
+//		a : 변수명을 의미있게 짓는 것이 좋다.
+//		d : 첫 문자는 소문자로 시작하되, 다른 단어가 붙을 경우 첫문자를 대문자로 표시.
+//		e : 첫 문자만 대문자로 해야한다.
+
+		//7. 다음중 변수 초기화가 잘못 된 것은?
+//		a. int b = 255;
+//		b. String s = "T"; 문자 혹은 문자열 ㄱㄴ
+//		c. char answer = 'no';
+//		d. char flag = 'F';
+//		답 : C
+		
+
+		//8. 다음 정수형 타입들 중에서 자바가 정수형 값을 만났을때 가장 기본적(대표적)으로 인식하는 변수 타입은?
+//		a. char
+//		b. int     정수: int 실수: double
+//		c. short
+//		d. long
+//		답 : b
+		
+	}
+
+}
