5fb9299c80f356a120fb689c9496375075cc4a91
jong-jin624 <jongjin1004baek@gmail.com>
2025-10-31 16:30:31 +0900

    퀴즈5 연산자, 변수입력

--- src/be_study/quiz/Quiz.java
+++ src/be_study/quiz/Quiz.java
@@ -17,7 +17,7 @@ public class Quiz {
 		String result = menuPrice <= 6000 ? "훌륭" : (menuPrice <= 8000 ? "선방" : (menuPrice <= 12000 ? "눈물" : "물로배채우기"));
 		System.out.println(result);
 		
-		System.out.println(13/4);
+
 		
 	}
 
